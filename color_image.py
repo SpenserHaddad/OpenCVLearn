faed66cb44f009430c5962f3a991ae7ff0a5cf65
Spenser Haddad <spenser.haddad@gmail.com>
2017-11-06 14:08:20 -0500

    Fixed RGB pictures, pictures are new saved

--- color_image.py
+++ color_image.py
@@ -1,27 +1,32 @@
 import sys
 import cv2
 
-rgb_image = cv2.imread(sys.argv[1])
-print("RGB value of pixel (20, 25): " + str(rgb_image[19, 24]))
-cv2.imshow("Original Image", rgb_image)
 
-rgb_planes = cv2.split(rgb_image)
-cv2.imshow("Blue", rgb_planes[0])
-cv2.imshow("Green", rgb_planes[1])
-cv2.imshow("Red", rgb_planes[2])
+def show_and_save_planes(image, labels):
+    planes = cv2.split(image)
+    for plane, label in zip(planes, labels):
+        cv2.imshow(label, plane)
+        cv2.imwrite(label + ".png", plane)
+
+
+rgb_image = cv2.imread(sys.argv[1], 1)
+cv2.imshow("Original Image", rgb_image)
+print("RGB value of pixel (20, 25): " + str(rgb_image[20, 25]))
+blue, green, red = rgb_image.copy(), rgb_image.copy(), rgb_image.copy()
+blue[:, :, 1:] = 0
+green[:, :, (0, 2)] = 0
+red[:, :, 0:2] = 0
+for plane, label in zip((blue, green, red), ("Blue", "Green", "Red")):
+    cv2.imshow(label, plane)
+    cv2.imwrite(label + ".png", plane)
+
 
 ycrcb_image = cv2.cvtColor(rgb_image, cv2.COLOR_BGR2YCrCb)
-print("YCrCb value of pixel (20, 25): " + str(ycrcb_image[19, 24]))
-ycrcb_planes = cv2.split(ycrcb_image)
-cv2.imshow("Y", ycrcb_planes[0])
-cv2.imshow("Cb", ycrcb_planes[1])
-cv2.imshow("Cr", ycrcb_planes[2])
+print("YCrCb value of pixel (20, 25): " + str(ycrcb_image[20, 25]))
+show_and_save_planes(ycrcb_image, ("Y", "Cr", "Cb"))
 
 hsv_image = cv2.cvtColor(rgb_image, cv2.COLOR_BGR2HSV)
-print("HSV value of pixel (20, 25): " + str(hsv_image[19, 24]))
-hsv_planes = cv2.split(hsv_image)
-cv2.imshow("Hue", hsv_planes[0])
-cv2.imshow("Saturation", hsv_planes[1])
-cv2.imshow("Value", hsv_planes[2])
+print("HSV value of pixel (20, 25): " + str(hsv_image[20, 25]))
+show_and_save_planes(hsv_image, ("Hue", "Saturation", "Value"))
 
 cv2.waitKey(0)
